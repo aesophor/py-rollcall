613e1b87df8de0e7e07bc08d825baa8f12399084
Marco Wang <m.aesophor@gmail.com>
2019-01-04 10:40:27 +0800

    Updated version number

--- pyrollcall/__init__.py
+++ pyrollcall/__init__.py
@@ -9,7 +9,7 @@ __copyright__ = "Copyright 2018-2019 Marco Wang (aesophor)"
 __license__ = "MIT"
 __maintainer__ = __author__
 __email__ = "m.aesophor@gmail.com"
-__version_info__ = (0, 1, 0)
+__version_info__ = (1, 0, 0)
 __version__ = '.'.join(str(e) for e in __version_info__)
 __description__ = "A Rollcall System using machine learning, OpenCV and PyGtk."
 
